61f427cbe9cc913c64533f4b69ee26dcfa47426d
Taalaibek Sagynaliev <sagynalievv.t@gmail.com>
2023-12-03 00:14:37 +0600

    add new components ChatCard

---
+++ src/components/ChatCard/ChatCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface Props {
+  author: string;
+  message: string;
+  datetime: string;
+}
+
+const ChatCard: React.FC<Props> = ({author, message, datetime}) => {
+  return (
+    <div className="card mb-3">
+      <div className="card-header">Sent at: {datetime}</div>
+      <div className="card-body">
+        <h5 className="card-title">Author: {author}</h5>
+        <p className="card-text">{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatCard;
